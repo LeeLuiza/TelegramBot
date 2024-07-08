e6a19b20324a7545766e394b17f9694810175eb3
LeeLuiza <Laurabeck1315@gmail.com>
2024-07-08 11:44:50 +0500

    commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from admin_handlers import admin_router
 
 
 async def main():
-    bot = Bot(token='6415106720:AAFIPE4jxKrGPbOnGUgfqX6L4HfEOQzXPFM')
+    bot = Bot(token='6646971882:AAF9yk580Lx-sKp2oCkouGFq907imQ2cd_c')
     dp = Dispatcher()
     dp.include_router(router)
     dp.include_router(admin_router)
